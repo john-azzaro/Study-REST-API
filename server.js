91ce767f20c2a0dc6149e3910ffc932e0d353d88
John Azzaro <jazzaro@temple.edu>
2019-08-09 13:01:33 -0700

    connecting to database

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
-const express = require('express');                       // load the express library
-const app = express();                                    // instantiate the app using express.
+const express = require('express');                                                // P2.1 -- Load the express library
+const app = express();                                                             // P2.2 -- Instantiate the app using express.
 
-app.listen(3000, function() {                             // listen on port 300 for 
-    console.log('Your app is litening on Port 3000...')
+const mongoose = require('mongoose');                                              // P3.1 -- Load mongoose module.
+mongoose.connect('mongodb://localhost/subscribers', { useNewUrlParser: true });    // P3.2 -- Connect to MongoDB database (& pass deprecation remedy for URL parser). 
+
+const db = mongoose.connection;                                                    // P3.3 -- Connect to your database.
+
+app.listen(3000, function() {                                                      // P2.3 -- listen on port 300 for 
+    console.log('Your app is litening on Port 3000...');
 });
