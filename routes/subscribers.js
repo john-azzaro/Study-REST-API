0e300a24d339b4ac0392e1a211d5f3c2a69c3feb
John Azzaro <jazzaro@temple.edu>
2019-08-10 12:29:07 -0700

    router setup

---
+++ routes/subscribers.js
@@ -0,0 +1,5 @@
+const express = require('express');                              //  P.5-5  --  Load the express library for this file.
+const router = express.Router();                                 //  P.5-5  --  Load the router part of express.
+
+
+module.exports = router;                                         //  P.5-5  --  Export router!
