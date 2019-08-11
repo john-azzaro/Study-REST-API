60e47f8b7c693ec72c830fdee013b2d3305710a2
John Azzaro <jazzaro@temple.edu>
2019-08-11 13:26:37 -0700

    schema export

--- models/subscribers.js
+++ models/subscribers.js
@@ -1,13 +1,17 @@
 const mongoose = require('mongoose');                            // P6  --  Load mongoose so that we can interact with the database.
-
-const subscriberSchema = new mongoose.Schema({
-    name: {
-
+ 
+const subscriberSchema = new mongoose.Schema({                   // P7  --  For the subscriberSchema, 
+    name: {                                                      // P7  --  The name must...
+        type: String,                                            // P7  --  ... the name must be a string
+        required: true                                           // P7  --  ... the name MUST be required (i.e. you must have a name)
     },
-    subscriberChannel: {
-
+    subscriberChannel: {                                         // P7  --  For the channel,
+        type: String,                                            // P7  --  ... the subscriberChannel must be a string
+        required: true                                           // P7  --  ... the subscriber MUST be required
     },
-    subscriberDate: {
-        
+    subscriberDate: {                                            // P7  --  For subscriber date,
+        type: Date,                                              // P7  --  ... the subscriber date must be a string
+        required: true,                                          // P7  --  ... the subscriber date must be required
+        default: Date.now                                        // P7  --  ... and the date of subscrption is the current date of subscription
     }
 });
