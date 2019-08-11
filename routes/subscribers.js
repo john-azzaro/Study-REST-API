164050a3490852f5a30fcabd3afa1c4178b37eac
John Azzaro <jazzaro@temple.edu>
2019-08-10 21:36:46 -0700

    added route outline

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -2,15 +2,29 @@ const express = require('express');                              //  P.5-5  --  
 const router = express.Router();                                 //  P.5-5  --  Load the router part of express.
 
 // GET ALL subscribers
+router.get('/', function(req, res) {                            
+
+});
+
+// GET One subscriber                                            
+router.get('/:id', function(req, res) {
+
+});
+
+// CREATE subscriber
 router.get('/', function(req, res) {
 
 });
 
-
-// GET One subscriber
-// CREATE subscriber
 // UPDATE subsrcriber
+router.get('/', function(req, res) {
+
+});
+
 // DELETE Subscriber
+router.get('/', function(req, res) {
+
+});
 
 
 module.exports = router;                                         //  P.5-5  --  Export router!
