f1eb2c870012d7da7c7d606a4e0b5fc24cc3dae7
John Azzaro <jazzaro@temple.edu>
2019-08-10 22:25:06 -0700

    finished route shell

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -2,29 +2,29 @@ const express = require('express');                              //  P.5-5  --  
 const router = express.Router();                                 //  P.5-5  --  Load the router part of express.
 
 // GET ALL subscribers
-router.get('/', function(req, res) {                            
+router.get('/', function(req, res) {                             //  P.6  --  Get all subscribers (on general route "/")  
 
 });
 
-// GET One subscriber                                            
-router.get('/:id', function(req, res) {
-
+// GET One subscriber                                             
+router.get('/:id', function(req, res) {                          //  P.6  --  Get one subscriber (using the id parameter)
+    
 });
 
 // CREATE subscriber
-router.get('/', function(req, res) {
+router.post('/', function(req, res) {                            //  P.6  -- Create a subscriber 
 
 });
 
 // UPDATE subsrcriber
-router.get('/', function(req, res) {
+router.patch('/:id', function(req, res) {                        //  P.6  -- Update a subscriber     
 
 });
 
 // DELETE Subscriber
-router.get('/', function(req, res) {
+router.delete('/:id', function(req, res) {                        //  P.6  --  Delete a subscriber 
 
 });
 
 
-module.exports = router;                                         //  P.5-5  --  Export router!
+module.exports = router;                                          //  P.5-5  --  Export router!
