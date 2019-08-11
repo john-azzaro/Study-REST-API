1ac346c92baefbbd23a8ced09b41df183ba52858
John Azzaro <jazzaro@temple.edu>
2019-08-10 21:34:32 -0700

    setting up routes

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,5 +1,16 @@
 const express = require('express');                              //  P.5-5  --  Load the express library for this file.
 const router = express.Router();                                 //  P.5-5  --  Load the router part of express.
 
+// GET ALL subscribers
+router.get('/', function(req, res) {
+
+});
+
+
+// GET One subscriber
+// CREATE subscriber
+// UPDATE subsrcriber
+// DELETE Subscriber
+
 
 module.exports = router;                                         //  P.5-5  --  Export router!
