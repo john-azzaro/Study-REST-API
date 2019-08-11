123e174c551a44d13d67b3abdd834ce9af707aea
John Azzaro <jazzaro@temple.edu>
2019-08-11 10:15:35 -0700

    schema build

---
+++ models/subscribers.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');                            // P6  --  Load mongoose so that we can interact with the database.
+
+const subscriberSchema = new mongoose.Schema({
+    name: {
+
+    },
+    subscriberChannel: {
+
+    },
+    subscriberDate: {
+        
+    }
+});
