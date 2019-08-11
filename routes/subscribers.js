c3aa144d8d21b50c1457706db7000f5ad3cd78f1
John Azzaro <jazzaro@temple.edu>
2019-08-11 09:14:30 -0700

    added models/model file

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -3,12 +3,12 @@ const router = express.Router();                                 //  P.5-5  --  
 
 // GET ALL subscribers
 router.get('/', function(req, res) {                             //  P.6  --  Get all subscribers (on general route "/")  
-    res.send('This is a test of the get all route...');
+    // res.send('This is a test');                                   P.6  --  Test GET (all) route functionality in Postman
 });
 
 // GET One subscriber                                             
 router.get('/:id', function(req, res) {                          //  P.6  --  Get one subscriber (using the id parameter)
-    
+    // res.send(req.params.id);                                      P.6  --  Test GET (one) route functionality in Postman
 });
 
 // CREATE subscriber
