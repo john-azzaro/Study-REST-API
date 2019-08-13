de8ca4dd25492a5ace2a47639617333886458a72
John Azzaro <jazzaro@temple.edu>
2019-08-13 16:23:57 -0700

    added async/await function to get all

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,10 +1,15 @@
-const express = require('express');                              //  P.5-5  --  Load the express library for this file.
-const router = express.Router();                                 //  P.5-5  --  Load the router part of express.
+const express = require('express');                              //  P.5  --  Load the express library for this file.
+const router = express.Router();                                 //  P.5  --  Load the router part of express.
 const Subscriber = require('../models/subscribers');             //  P7  --  Include your model in this main route file (note the ".." so that you go back one folder to use "models").
 
 // GET ALL subscribers
-router.get('/', function(req, res) {                             //  P.6  --  Get all subscribers (on general route "/")  
-    // res.send('This is a test');                                   P.6  --  Test GET (all) route functionality in Postman
+router.get('/', async function(req, res) {                       //  P.6/8  --  Get all subscribers (on general route "/") and add the "async" modifer 
+    try {                                                        //  P.8  --  Add a "try" block for your route.
+        const subscribers = await Subscriber.find();             //  P.8  --  ... Await all the subscribers we have.
+        res.json(subscribers);                                   //  P.8  --  ... and send a response of the subscribers if successful.
+    } catch (err) {                                              //  P.8  --  Add a "catch" block incase there are any errors and pass "err" object.
+        res.status(500).json( {message: err.message } );         //  P.8  --  ... send a status of 500 (error is database fault) and the json message, which will show the err object.
+    }
 });
 
 // GET One subscriber                                             
